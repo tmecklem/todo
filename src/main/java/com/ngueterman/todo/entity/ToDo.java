620024d25a61ff4a530f478cf5df822e44065b6f
Nick Gueterman <nick_gueterman@yahoo.com>
2012-03-21 16:58:14 -0400

    project files

---
+++ src/main/java/com/ngueterman/todo/entity/ToDo.java
@@ -0,0 +1,56 @@
+package com.ngueterman.todo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ToDo {
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column (length=1000) private String text;
+	private Date dueDate;  
+	private Boolean complete;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Boolean getComplete() {
+		return complete;
+	}
+
+	public void setComplete(Boolean complete) {
+		this.complete = complete;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+
+}
